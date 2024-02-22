030c194e87e71a9fc6f0f1b9586970fc3fc34b03
dcortesnet <dcortes.net@gmail.com>
2024-02-22 13:30:14 -0300

    Order

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { SignInPage } from "./pages/public/SignInPage";
 import { LoginPage } from "./pages/public/LoginPage";
 import { DashboardPage } from "./pages/private/DashboardPage";
 import { NotFoundPage } from "./pages/public/NotFoundPage";
-import "./App.css";
 import { HomePage } from "./pages/private/HomePage";
 import { ArticlesListPage } from "./pages/private/ArticlesListPage";
 import { UserListPage } from "./pages/private/UsersListPage";
-import ArticlesDetailPage from "./pages/private/ArticlesDetailPage";
+import { ArticlesDetailPage } from "./pages/private/ArticlesDetailPage";
 import { UserDetailPage } from "./pages/private/UsersDetailPage";
+import "./App.css";
 
 const PrivateRoutes = () => {
   let auth = { token: true };
